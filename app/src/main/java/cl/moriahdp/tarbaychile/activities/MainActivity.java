84a42853b37105be74bbf4c70e229fb7eedeb18e
Edwin Peraza <edwinperaza87@gmail.com>
2016-07-31 17:51:13 -0400

    Add Toolbar at the bottom of the screen and internet permission
    - Toolbar color is white and has two icons.
    - Delete action bar (temporal)
    - Set Toolbar to be available to several activities

--- app/src/main/java/cl/moriahdp/tarbaychile/activities/MainActivity.java
+++ app/src/main/java/cl/moriahdp/tarbaychile/activities/MainActivity.java
@@ -1,7 +1,9 @@
 package cl.moriahdp.tarbaychile.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 import cl.moriahdp.tarbaychile.R;
 
@@ -11,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
