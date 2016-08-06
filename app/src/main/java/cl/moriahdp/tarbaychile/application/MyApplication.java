a110f1713465f481001b02d5c229f50e8641edd7
edwinperaza <edwinperaza87@gmail.com>
2016-08-06 12:07:26 -0400

    Add Facebook Basic Login

---
+++ app/src/main/java/cl/moriahdp/tarbaychile/application/MyApplication.java
@@ -0,0 +1,11 @@
+package cl.moriahdp.tarbaychile.application;
+
+import android.app.Application;
+
+import com.facebook.FacebookSdk;
+
+/**
+ * Created by edwinperaza on 8/6/16.
+ */
+public class MyApplication extends Application {
+}
