87190bd80b1cf260a8d7955518c72cc0f2330650
edwinperaza <edwinperaza87@gmail.com>
2016-08-20 01:19:26 -0300

    Add field to user and method to get data from JSON

--- app/src/main/java/cl/moriahdp/tarbaychile/models/user/User.java
+++ app/src/main/java/cl/moriahdp/tarbaychile/models/user/User.java
@@ -1,5 +1,8 @@
 package cl.moriahdp.tarbaychile.models.user;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by edwinmperazaduran on 7/31/16.
  */
@@ -8,14 +11,30 @@ public class User {
     private String email;
     private String username;
     private String password;
+    private String token;
 
     public User() {
     }
 
-    public User(String email, String username, String password) {
+    public User(String email, String username, String password, String token) {
         this.email = email;
         this.username = username;
         this.password = password;
+        this.token = token;
+    }
+
+    public static User jsonObjectToUser (JSONObject jsonObjectUser) {
+        User user = new User();
+
+        try {
+            user.setUsername(jsonObjectUser.getString(UserRequestManager.USERNAME));
+            user.setEmail(jsonObjectUser.getString(UserRequestManager.EMAIL));
+            user.setPassword(jsonObjectUser.getString(UserRequestManager.PASSWORD));
+            user.setToken(jsonObjectUser.getString(UserRequestManager.TOKEN));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return user;
     }
 
     public String getEmail() {
@@ -40,5 +59,12 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
     }
 }
